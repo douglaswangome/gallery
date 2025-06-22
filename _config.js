26a9f0dc3f4ff0f1e8a1061212c8cc3a10bb1c38
Douglas Wangome <ishdagnesh@gmail.com>
2025-06-22 22:11:52 +0300

    Add initial project configuration files and update MongoDB connection settings

--- _config.js
+++ _config.js
@@ -1,9 +1,17 @@
 var config = {}
 
 // Update to have your correct username and password
+// config.mongoURI = {
+// 	production: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom?retryWrites=true&w=majority',
+// 	development: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
+// 	test: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom-test?retryWrites=true&w=majority',
+// }
+
+// Updated configuration for MongoDB connection
 config.mongoURI = {
-    production: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom?retryWrites=true&w=majority',
-    development: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom-dev?retryWrites=true&w=majority',
-    test: 'mongodb+srv://<USERNAME>:<PASSWORD>@gallery.wc344.mongodb.net/darkroom-test?retryWrites=true&w=majority',
+	production: "mongodb+srv://admin:12345@main.c79awyw.mongodb.net/?retryWrites=true&w=majority&appName=main",
+	development: "mongodb+srv://admin:12345@main.c79awyw.mongodb.net/?retryWrites=true&w=majority&appName=main",
+	test: "mongodb+srv://admin:12345@main.c79awyw.mongodb.net/?retryWrites=true&w=majority&appName=main",
 }
+
 module.exports = config;
